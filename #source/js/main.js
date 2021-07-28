44434b58215a10722dcc3309eb0f779d3f463f41
Zen-Hub <inboxshow1@gmail.com>
2021-07-28 23:10:49 +0300

    added board in js

--- #source/js/main.js
+++ #source/js/main.js
@@ -2,5 +2,21 @@
 // @prepros-append script.js
 // @prepros-append end.js
 
-console.log('GOONE' )
-console.log('GO')
+const board = document.querySelector('#board');
+const colors = ['Aqua', 'Cyan', 'Gold', 'OrangeRed', 'Fuchsia', 'Lime', 'RosyBrown', 'SteelBlue'];
+
+const SQUARES_NUMBER = 500;
+
+for(let i =0;i<SQUARES_NUMBER;i++){
+
+    const square = document.createElement('div')
+    square.classList.add('square');
+
+    square.addEventListener('mouseover',()=>
+    setColor('square'));
+
+    square.addEventListener('mouseleave',()=>
+    removeColor('square'));
+
+    board.append(square);
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 
-console.log('GOONE' )
-console.log('GO')
+const board = document.querySelector('#board');
+const colors = ['Aqua', 'Cyan', 'Gold', 'OrangeRed', 'Fuchsia', 'Lime', 'RosyBrown', 'SteelBlue'];
 
-console.log('MrRobot!       ')
-console.log('end')
+const SQUARES_NUMBER = 500;
+
+for(let i =0;i<SQUARES_NUMBER;i++){
+
+    const square = document.createElement('div')
+    square.classList.add('square');
+
+    square.addEventListener('mouseover',()=>
+    setColor('square'));
+
+    square.addEventListener('mouseleave',()=>
+    removeColor('square'));
+
+    board.append(square);
+}
+
+
