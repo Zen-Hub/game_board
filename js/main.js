a24067e2a2793c34e93f72abc2321806823f6061
Zen-Hub <inboxshow1@gmail.com>
2021-07-29 20:46:22 +0300

    added function getRandom color

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
 
-const board = document.querySelector('#board');
-const colors = ['Aqua', 'Cyan', 'Gold', 'OrangeRed', 'Fuchsia', 'Lime', 'RosyBrown', 'SteelBlue'];
+const board = document.querySelector('#board')
+const colors = ['Aqua', 'Cyan', 'Gold', 'OrangeRed', 'Fuchsia', 'Lime', 'RosyBrown', 'SteelBlue']
+SQUARES_NUMBER = 500
 
-const SQUARES_NUMBER = 500;
-
-for(let i =0;i<SQUARES_NUMBER;i++){
-
+for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div')
-    square.classList.add('square');
+    square.classList.add('square')
 
-    square.addEventListener('mouseover',()=>
-    setColor('square'));
+    square.addEventListener('mouseover', () =>
+        setColor(square))
 
-    square.addEventListener('mouseleave',()=>
-    removeColor('square'));
+    square.addEventListener('mouseleave', () =>
+        removeColor(square))
 
-    board.append(square);
+
+    board.append(square)
 }
 
-function setColor(element){
-    const color = getRandomColor();
-    element.style.backgroundColor = color;
-    element.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`;
+function setColor(element) {
+    const color = getRandomColor()
+
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element){
-element.style.backgroundColor = '#1d1d1d';
-element.style.boxShadow = ` 0 0 2px #000`
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
 }
 
-
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
